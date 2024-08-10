a6fa69e933909c01cafbdc3fd0e9db3414b5eda5
dolwinf <dolwinf@gmail.com>
2024-08-11 09:35:21 +1000

    Run python directly

--- main.py
+++ main.py
@@ -106,5 +106,4 @@ async def download_video(request: Request, background_tasks: BackgroundTasks):
 app.include_router(router)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port="$PORT")
+    app.run(host="0.0.0.0", port="$PORT")
