aace5f02f4eaa531fedb126a1809cd006fded964
dolwinf <dolwinf@gmail.com>
2024-08-10 23:15:33 +1000

    Fix port

--- main.py
+++ main.py
@@ -72,4 +72,4 @@ async def download_video(request: Request, background_tasks: BackgroundTasks):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
